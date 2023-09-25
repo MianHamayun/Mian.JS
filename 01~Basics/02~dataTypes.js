e9c1dcda2b67c89ddddbbf388ec78b93176a50a0
MianHamayun <mianhamayun9@gmail.com>
2023-09-25 13:07:38 +0500

    datatypes

---
+++ 01~Basics/02~dataTypes.js
@@ -0,0 +1,36 @@
+"use strict" // Treat all JS code as newer version.
+
+// alert("hello"); //because we are working in node. this will throw error.
+
+// alert( 3 + 3 ); we are using NodeJS, not browser
+
+// console.log(3 + 3); console.log("hello") //bad practice. Code Readability should be high.
+
+let name = "hello"
+let age = 20
+let isLoggenIn = false
+let state;
+
+
+// number => 2 to 53
+// bigint 
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+
+
+
+// object
+
+
+// console.log(typeof "hello");
+// console.log(typeof age);
+// console.log(typeof null); //object
+// console.log(typeof undefined); //undefined
+
+
+
+
