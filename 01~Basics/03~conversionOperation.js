ef9654a8e24f7f19fd27b7e280c0ed1730f79f62
MianHamayun <mianhamayun9@gmail.com>
2023-09-25 13:48:58 +0500

    Conversion of datatypes

---
+++ 01~Basics/03~conversionOperation.js
@@ -0,0 +1,45 @@
+let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "hello"
+
+
+
+
+
+// const {score} = req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score) //it will return number for sure
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+// let isLoggenIn = true
+// let isLoggenIn = 1
+let isLoggenIn = "hello"
+
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// hitesh => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber); 
+console.log(typeof stringNumber); 
+
+
