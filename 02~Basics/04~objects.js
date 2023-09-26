5dfb4a76ff36b22ec89e4fcf0bdfc5ae728b98e7
MianHamayun <mianhamayun9@gmail.com>
2023-09-26 16:22:44 +0500

    objects part 2

---
+++ 02~Basics/04~objects.js
@@ -0,0 +1,76 @@
+// **************** Singleton Object **************
+
+// const threadsUser = new Object()
+
+
+
+// ****************** Non Singleton Object *******************
+
+const threadsUser = {};
+
+
+threadsUser.id = "5589jon"
+threadsUser.name = "Johnny"
+threadsUser.isLoggedIn = false
+
+
+// console.log(threadsUser);
+
+
+
+const regularUser = {
+  email: "john@email.com",
+  fullname: {
+    userfullname: {
+      firstname: "John",
+      lastname: "Doe"
+    }
+  }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "c", 4: "d" }
+
+// const obj3 = { obj1, obj2 }
+
+
+// Object Assign works at {target, source}
+// it is good practice to target empty object {} and the other objects are source obviously.
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+// will use Spread the most
+
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3);
+
+
+
+
+const users = [
+  {
+    id: 1,
+    email: "h@gmail.com"
+  },
+  {
+
+  },
+  {
+
+  }
+]
+
+users[1].email;
+console.log(threadsUser);
+
+
+console.log(Object.keys(threadsUser));
+console.log(Object.values(threadsUser));
+console.log(Object.entries(threadsUser));
+
+
+
+console.log(threadsUser.hasOwnProperty('isLoggedIn'));
