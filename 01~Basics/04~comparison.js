71cf11b6d07536aac4cd30cabfa8d8da0a3a52f9
MianHamayun <mianhamayun9@gmail.com>
2023-10-03 17:04:00 +0500

    update

--- 01~Basics/04~comparison.js
+++ 01~Basics/04~comparison.js
@@ -4,8 +4,8 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
-// console.log("2" > 1); //true
-// console.log("02" > 1); //true
+console.log("2" > 1); //true
+console.log("02" > 1); //true
 
 
 // console.log(null > 0); //false *Comparisons convert null into a number,trating it as 0*
@@ -23,3 +23,5 @@
 // ===
 
 console.log("2" === 1); // checks datatype as well
+
+console.log('2' == 2);
